83f5bddc2b3d24acc37af0b643aa367db0f01a20
giantliao <giantliao@tutanota.com>
2020-10-24 15:21:28 +0800

    add to repo

--- resource/pacserver/pacserver.go
+++ resource/pacserver/pacserver.go
@@ -6,9 +6,7 @@ import (
 	"github.com/giantliao/beatles-client-lib/config"
 	"github.com/giantliao/beatles-client-lib/resource/paclist"
 	"github.com/giantliao/beatles-client-lib/resource/webfs"
-	"io/ioutil"
 	"log"
-	"path"
 	"strconv"
 
 	"net/http"
@@ -40,27 +38,27 @@ func StartWebDaemon() {
 
 }
 
-func StartWebDaemon2() {
-	cfg := config.GetCBtlc()
-	if err := paclist.RestoreAssets(cfg.GetScriptPath(), "resource/script"); err != nil {
-		log.Println("restore script file failed", err)
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/pac", func(w http.ResponseWriter, r *http.Request) {
-		byt, err := ioutil.ReadFile(path.Join(cfg.GetScriptPath(), "resource/script/gfw.js"))
-		if err != nil {
-			panic(err)
-		}
-		w.Write(byt)
-	})
-
-	addr := ":" + strconv.Itoa(config.GetCBtlc().StreamServerPacPort)
-	log.Println("PAC Server Start at", addr)
-	webserver = &http.Server{Addr: addr, Handler: mux}
-	log.Fatal(webserver.ListenAndServe())
-}
+//func StartWebDaemon2() {
+//	cfg := config.GetCBtlc()
+//	if err := paclist.RestoreAssets(cfg.GetScriptPath(), "resource/script"); err != nil {
+//		log.Println("restore script file failed", err)
+//	}
+//
+//	mux := http.NewServeMux()
+//
+//	mux.HandleFunc("/pac", func(w http.ResponseWriter, r *http.Request) {
+//		byt, err := ioutil.ReadFile(path.Join(cfg.GetScriptPath(), "resource/script/gfw.js"))
+//		if err != nil {
+//			panic(err)
+//		}
+//		w.Write(byt)
+//	})
+//
+//	addr := ":" + strconv.Itoa(config.GetCBtlc().StreamServerPacPort)
+//	log.Println("PAC Server Start at", addr)
+//	webserver = &http.Server{Addr: addr, Handler: mux}
+//	log.Fatal(webserver.ListenAndServe())
+//}
 
 func StopWebDaemon() {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
